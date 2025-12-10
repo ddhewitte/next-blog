7d7f2cf5ef41ead164ce3c0898f85fbb09a3cdfa
adjihyphen <adji.hendrawan@moneyherogroup.com>
2025-12-10 20:49:22 +0700

    Routing : page and contact

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <>
+      <div className="text-center mb-12">
+        About ..
+      </div>
+    </>
+    
+  );
+}
---
+++ app/contact/page.tsx
@@ -0,0 +1,10 @@
+export default function Contact() {
+  return (
+    <>
+      <div className="text-center mb-12">
+        Contact page ..
+      </div>
+    </>
+    
+  );
+}
