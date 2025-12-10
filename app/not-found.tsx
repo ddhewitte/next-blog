10c2b8a84177925308104d43d8f196ac68cf6469
adjihyphen <adji.hendrawan@moneyherogroup.com>
2025-12-10 20:27:43 +0700

    modified 404 not found

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+export default function NotFound(){
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-center p-10">
+        <h1 className="text-4xl font-bold mb-4">404 — Page Not Found</h1>
+        <p className="text-slate-600 mb-6">
+            Sorry page not found, please try again next time
+        </p>
+        
+        </div>
+    );
+}
